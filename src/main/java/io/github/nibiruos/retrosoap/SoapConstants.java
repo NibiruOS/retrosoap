338a3b9a12479541575180be9daad9ebd97d8da5
Lautaro Brasseur <lbrasseur@gmail.com>
2018-10-07 02:13:33 -0300

    SOAP constants for Retrofit services

---
+++ src/main/java/io/github/nibiruos/retrosoap/SoapConstants.java
@@ -0,0 +1,6 @@
+package io.github.nibiruos.retrosoap;
+
+public interface SoapConstants {
+    String CONTENT_TYPE_HEADER = "Content-Type: text/xml;charset=UTF-8";
+    String SOAP_ACTION_PREFIX = "SOAPAction: ";
+}
